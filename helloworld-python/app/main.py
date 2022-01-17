6c0bb0703bfa93c6f151f3a615682e14eab850a0
weiweilonglong <86655800+weiweilonglong@users.noreply.github.com>
2022-01-17 10:03:44 +0800

    微信云托管测试文件
    这是一个微信云托管的开始

---
+++ helloworld-python/app/main.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Welcome to CloudBase!\n'
+
+if __name__ == "__main__":
+    #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
